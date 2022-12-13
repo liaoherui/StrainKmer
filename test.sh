6c61a580452d1a59d5e233ce06799d93990da0b7
Ray <851836818@qq.com>
2022-12-13 20:37:26 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-python StrainKmer_for_cmd.py -i Test_data_and_db/FastQ_data/ -d Test_data_and_db/SCOV2_nextstrain/SCOV2 -o Test_outdir
+python StrainKmer_for_cmd.py -i Test_data_and_db/FastQ_data/ -d Test_data_and_db/SCOV2/SCOV2 -o Test_outdir
